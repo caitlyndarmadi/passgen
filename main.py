17e36516ef484df9dc99da8d37ab460e46682f26
caitlyndarmadi <darmadicaitlyn@gmail.com>
2025-01-03 21:52:48 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import random
 
-chars = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-random_string = ""
-x = int(input("Mau berapa password?"))
-for i in range(x):
-    random_string += random.choice(chars)
-print(random_string)
+def gen_pass(pass_length):
+    elements = "+-/*!&$#?=@<>"
+    password = ""
+
+    for i in range(pass_length):
+        password += random.choice(elements)
+
+    return password
